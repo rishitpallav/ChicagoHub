4fc442d8851e906581ff9498cfc3ae9bebdf81f7
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2023-01-06 17:34:10 -0600

    Search, Yelp Reviews, Divvy Stations

---
+++ frontend/src/app/components/divvy-heat-map-realtime/heat-map-realtime.component.ts
@@ -0,0 +1,430 @@
+import { AfterViewInit, Component,Input,ElementRef, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import * as heatmap from 'heatmap.js'
+import { google } from 'google-maps';
+
+import { Station } from '../../station';
+import { PlacesService } from '../../places.service';
+import { Observable } from "rxjs";
+import 'rxjs/add/observable/interval';
+import { Subscription } from 'rxjs/Subscription';
+import * as moment from 'moment';
+
+interface Location {
+  lat: number;
+  lng: number;
+  zoom: number;
+  address_level_1?:string;
+  address_level_2?: string;
+  address_country?: string;
+  address_zip?: string;
+  address_state?: string;
+  label: string;
+}
+
+@Component({
+  selector: 'heatmap-realtime',
+  templateUrl: './heat-map-realtime.component.html',
+  styleUrls: ['./heat-map-realtime.component.scss']
+})
+
+
+export class HeatMapRealtimeComponent implements OnInit{
+
+    // The following are tunable parameters that are used to display n samples
+    // of divvy data on the heatMap
+
+    FRAMES_PER_HOUR = 30;
+    FRAMES_PER_DAY = 24;
+    FRAMES_PER_WEEK = 7;
+
+    PAST_HOUR =  'Past Hour';
+    PAST_24_HOURS =  'Last 24 Hours';
+    PAST_7_DAYS =  'Last 7 Days';
+
+    newTimeRangeSelection = true;
+    notNewTimeRangeSelection = false;
+
+    newTimeHourStart = 0;
+    newTimeHourEnd = 4;
+
+    timeRangeSelected = this.PAST_HOUR;
+
+    google: google;
+    gradient;
+    gradientStep = -1;
+    newGradient;
+    distinct = [];
+    stations:Station[];
+    heatMapData: any[];
+    timeArray:any;
+    timeOffset = 0;
+    timer;
+
+    noOfDivvyDataSamplesRequested: number;
+    noOfDivvyDataSamplesProcessed: number;
+
+    currentChicagoTime;
+
+    timeStamp;
+    currentTime;
+
+    timeValues = [
+      { id : this.PAST_HOUR, value: this.PAST_HOUR},
+      { id : this.PAST_24_HOURS, value: this.PAST_24_HOURS},
+      { id : this.PAST_7_DAYS, value: this.PAST_7_DAYS}
+    ];
+
+    hourValues1 = [
+      { value : "12 AM", id: "12"},
+      { value : "1 AM", id: "1"},
+      { value : "2 AM", id: "2"},
+      { value : "3 AM", id: "3"},
+      { value : "4 AM", id: "4"},
+      { value : "5 AM", id: "5"},
+      { value : "6 AM", id: "6"},
+      { value : "7 AM", id: "7"},
+      { value : "8 AM", id: "8"},
+      { value : "9 AM", id: "9"},
+      { value : "10 AM", id: "10"},
+      { value : "11 AM", id: "11"},
+      { value : "12 PM", id: "12"},
+      { value : "1 PM", id: "13"},
+      { value : "2 PM", id: "14"},
+      { value : "3 PM", id: "15"},
+      { value : "4 PM", id: "16"},
+      { value : "5 PM", id: "17"},
+      { value : "6 PM", id: "18"},
+      { value : "7 PM", id: "19"},
+      { value : "8 PM", id: "20"},
+      { value : "9 PM", id: "21"},
+      { value : "10 PM", id: "22"},
+      { value : "11 PM", id: "23"}
+    ];
+
+    hourValues2 = [
+      { value : "1 AM", id: "1"},
+      { value : "2 AM", id: "2"},
+      { value : "3 AM", id: "3"},
+      { value : "4 AM", id: "4"},
+      { value : "5 AM", id: "5"},
+      { value : "6 AM", id: "6"},
+      { value : "7 AM", id: "7"},
+      { value : "8 AM", id: "8"},
+      { value : "9 AM", id: "9"},
+      { value : "10 AM", id: "10"},
+      { value : "11 AM", id: "11"},
+      { value : "12 PM", id: "12"},
+      { value : "1 PM", id: "13"},
+      { value : "2 PM", id: "14"},
+      { value : "3 PM", id: "15"},
+      { value : "4 PM", id: "16"},
+      { value : "5 PM", id: "17"},
+      { value : "6 PM", id: "18"},
+      { value : "7 PM", id: "19"},
+      { value : "8 PM", id: "20"},
+      { value : "9 PM", id: "21"},
+      { value : "10 PM", id: "22"},
+      { value : "11 PM", id: "23"},
+      { value : "12 AM", id: "24"}
+    ];
+
+    private map: google.maps.Map = null;
+    heatmap: google.maps.visualization.HeatmapLayer = null;
+
+
+    constructor(private placesService:PlacesService) {
+
+    }
+
+
+    ngOnInit() {
+      this.timeRangeSelected = this.PAST_HOUR;
+      this.noOfDivvyDataSamplesProcessed=2;
+    }
+
+
+
+
+  
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+////////////////   Angular Callback for TimeRangeSelection
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+  changeTimeRangeSelected(data){
+    console.log('this.timeRangeSelected', this.timeRangeSelected);
+    console.log('new Date().getTime()', new Date().getTime());
+    console.log('newTimeHourStart', this.newTimeHourStart);
+    console.log('newTimeHourEnd', this.newTimeHourEnd);
+    // console.log('new Date()', new Date().setHours(12,0,0));
+    // console.log('new Date()', new Date().setHours(13,0,0));
+    this.clearHeatMap();
+    this.noOfDivvyDataSamplesProcessed = 0;
+    clearTimeout(this.timer);
+    this.getDivvyStationsStatus(this.newTimeRangeSelection);
+
+
+
+  }
+
+
+  
+
+  //////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////
+  // A function used to prefix a single digit with a ZERO
+
+
+  checkForSingleDigitAndPrefixZeroIfNeeded(digitValue) {
+
+    if (digitValue < 10) 
+      digitValue = "0" + digitValue;
+    
+    return digitValue;
+  }
+
+
+
+  ///////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////
+
+  getDivvyStationsStatus(timeRangeSelection){
+
+    
+    // The algorithm to collect log data for Divvy stations as follows:
+    // Get current time and then rewind backward in time based on the time selection
+    // 1 Hour  time selection     :  -60 * 60 * 1000 seconds
+    // 24 hours  time selection   :  24 * -60 * 60 * 1000 seconds
+    // 7 days time selection      :   7 * 24 * -60 * 60 * 1000 seconds
+
+    // this is the time used to be displayed as label on the map
+    var simulatedClockTime;
+
+    var currentTimeStart = new Date();
+    currentTimeStart.setHours(this.newTimeHourStart,0,0,0);
+    // console.log('currentTimStart', currentTimeStart);
+
+    var currentTimeEnd= new Date();
+    currentTimeEnd.setHours(this.newTimeHourEnd,0,0,0);
+    // console.log('currentTimeEnd', currentTimeEnd);
+
+    var timeDiff = this.newTimeHourEnd - this.newTimeHourStart;
+
+    var startTimeForDataCollection ;
+    var currentTimeForDataCollection ;
+
+
+
+    // What is the time-range selected? 1-Hour, 24-Hours, 7-days?
+    // Based on the time-range selected we specify how many animation frames to display on th eheatmap 
+    // for the Divvy logs collected from ElasticSearch
+    // every data sample is displayed as a frame on the heatmap
+
+    if (this.timeRangeSelected == this.PAST_HOUR){
+      this.noOfDivvyDataSamplesRequested = this.FRAMES_PER_HOUR;
+      startTimeForDataCollection = new Date(currentTimeEnd.getTime()- timeDiff * 60 * 60 * 1000);
+      currentTimeForDataCollection = new Date(startTimeForDataCollection.getTime()+this.timeOffset);
+    } 
+    else if (this.timeRangeSelected == this.PAST_24_HOURS){
+      this.noOfDivvyDataSamplesRequested = this.FRAMES_PER_DAY;
+      startTimeForDataCollection = new Date(currentTimeEnd.getTime() - 24* 60 * 60 * 1000);
+      currentTimeForDataCollection = new Date(startTimeForDataCollection.getTime()+this.timeOffset);
+    } 
+
+    else if (this.timeRangeSelected == this.PAST_7_DAYS){
+      this.noOfDivvyDataSamplesRequested = this.FRAMES_PER_WEEK;
+      startTimeForDataCollection = new Date(currentTimeEnd.getTime() - 7*24* 60 * 60 * 1000);
+      currentTimeForDataCollection = new Date(startTimeForDataCollection.getTime()+this.timeOffset);
+    }
+
+
+
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+
+
+    /////////////     ADD YOUR CODE HERE      ///////////
+    
+    // Write your code to extend the above if statement with else block to handle
+    //          timeRangeSelected == this.PAST_24_HOURS
+    //          noOfDivvyDataSamplesRequested = this.FRAMES_PER_DAY
+    // And
+    //          timeRangeSelected == this.PAST_7_DAYS
+    //          noOfDivvyDataSamplesRequested = this.FRAMES_PER_WEEK
+
+
+    // The following are fine-tunable parameters 
+    // They are used to specify how many data samples (Divvy heart-beats per Time-Unit)
+    //      FRAMES_PER_HOUR = 30;
+    //      FRAMES_PER_DAY = 24;
+    //      FRAMES_PER_WEEK = 7;
+
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+
+
+
+
+
+
+    //console.log('this.noOfDivvyDataSamplesRequested = ', this.noOfDivvyDataSamplesRequested);
+    //console.log('startTimeForDataCollection= ', startTimeForDataCollection);
+    //console.log('currentTimeForDataCollection= ', currentTimeForDataCollection);
+    //console.log('this.timeRangeSelected = ', this.timeRangeSelected);
+
+
+
+    // Check to see if we collected the REQUESTED number of samples from Divvy logs on ElasticSearch
+    // For example, for the past hour we collect 30 samples (2 minutes increments) and display 30 heatmap frames
+    // For example, for the past 24 hours we collect 24 samples (1 hour increments) and display 24 heatmap frames
+
+
+    if(this.noOfDivvyDataSamplesProcessed < this.noOfDivvyDataSamplesRequested){
+      // console.log('timerangeselected ln290', this.timeRangeSelected);
+      // console.log('timerangeselection ln290', timeRangeSelection);
+      this.placesService.get_all_divvy_stations_data_range(this.timeRangeSelected, timeRangeSelection, currentTimeEnd, timeDiff).subscribe((data: Station[]) => {
+        //console.log('getDivvyStationsStatus -- noOfDivvyDataSamplesProcessed=', this.noOfDivvyDataSamplesProcessed);
+        this.noOfDivvyDataSamplesProcessed = this.noOfDivvyDataSamplesProcessed + 1;
+
+
+        // Adjust time Offset for the next cycle by 2 minutes
+        // for example: 2, 4, 6, 8, 10, ...
+        // So, current time is 
+        if (this.timeRangeSelected == this.PAST_HOUR){
+          this.timeOffset = (this.noOfDivvyDataSamplesProcessed) * (60 * 1000 * 2 * timeDiff);
+        } else 
+                if (this.timeRangeSelected == this.PAST_24_HOURS){
+                  this.timeOffset = (this.noOfDivvyDataSamplesProcessed) * (60 * 60 * 1000);
+                } else 
+                        if (this.timeRangeSelected == this.PAST_7_DAYS){
+                          this.timeOffset = (this.noOfDivvyDataSamplesProcessed) * (24 * 60 * 60 * 1000);
+                        }
+
+        // Create digital clock as label on the heatmap
+        // The convention is to prefix with ZERO the single digit for month, day, jour, minute, second
+        // in the digital clock/time
+
+        //The value returned by getMonth() is an integer between 0 and 11. 
+        // 0 corresponds to January, 1 to February
+        currentTimeForDataCollection = new Date(startTimeForDataCollection.getTime()+this.timeOffset);
+        let monthNumber = this.checkForSingleDigitAndPrefixZeroIfNeeded(currentTimeForDataCollection.getMonth()+ 1) ;
+        simulatedClockTime = currentTimeForDataCollection.getFullYear() +'-'+ 
+                              monthNumber + '-' +
+                              this.checkForSingleDigitAndPrefixZeroIfNeeded(currentTimeForDataCollection.getDate()) + '\t' +
+                              this.checkForSingleDigitAndPrefixZeroIfNeeded(currentTimeForDataCollection.getHours()) + ':' +
+                              this.checkForSingleDigitAndPrefixZeroIfNeeded(currentTimeForDataCollection.getMinutes()) + ':' +
+                              this.checkForSingleDigitAndPrefixZeroIfNeeded(currentTimeForDataCollection.getSeconds());
+
+        this.timeStamp = simulatedClockTime;
+
+
+
+        // Now clear the HeatMap and then plot the data on the heatmap
+        this.clearHeatMap();
+        this.plot_availableDocksInDivvyStations_on_heatMap(data);
+        
+
+        // Not a new time-range selection, so continue calling the same method but 
+        // with new time offset increment
+        this.timer = setTimeout(() =>  this.getDivvyStationsStatus(this.notNewTimeRangeSelection) , 300);
+      });
+    }
+
+    // we are done ... reset time offset to zero
+    this.timeOffset = 0;
+  }
+
+  ///////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////
+
+  plot_availableDocksInDivvyStations_on_heatMap(availableDocksInDivvyStations){
+
+    let locationsOfAvailableDocksInDivvyStations=[];
+    //console.log(availableDocksInDivvyStations);
+
+    // var newDocks=[];
+    // var x = 0;
+    // for(let i = 0; i < availableDocksInDivvyStations.length; i++) {
+    //   var availableDocks = availableDocksInDivvyStations[i].availableDocks;
+    //   var availableBikes = availableDocksInDivvyStations[i].availableBikes;
+    //   // console.log('availableBikes', availableBikes);
+    //   // console.log('availableDocks', availableDocks);
+    //   if(availableBikes/(availableBikes + availableDocks) > 0.5 ) {
+    //     this.stations[x] = availableDocksInDivvyStations[i];
+    //     x++;
+    //   }
+    // }
+
+    this.stations = availableDocksInDivvyStations;
+    //console.log("data",this.stations.length);
+
+    for (let i = 0; i < this.stations.length; i++) {
+      let divvy_dock_station_location = {
+        location: new google.maps.LatLng(this.stations[i].latitude, this.stations[i].longitude),
+        weight: this.stations[i].availableDocks
+      }
+      locationsOfAvailableDocksInDivvyStations.push(divvy_dock_station_location);
+    }
+    // console.log(locationsOfAvailableDocksInDivvyStations);
+
+    this.heatMapData = locationsOfAvailableDocksInDivvyStations;
+
+    this.heatmap = new google.maps.visualization.HeatmapLayer({
+      data: this.heatMapData
+    });
+
+    this.heatmap.setMap(this.map);
+  }
+
+
+
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+
+  public location:Location = {
+    lat: 41.882607,
+    lng: -87.643548,
+    label: 'You are Here',
+    zoom: 10
+  };
+
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+  onMapLoad(mapInstance: google.maps.Map) {
+    // default display is Past Hour data
+    this.timeRangeSelected = this.PAST_HOUR;
+
+    this.map = mapInstance;
+
+    this.getDivvyStationsStatus(true);
+
+  }
+
+
+  clearHeatMap(){
+    if(this.heatmap){
+      this.heatmap.setMap(null);
+      this.heatMapData =[];
+    }
+
+  }
+
+
+  ngOnDestroy() {
+
+      this.map  = null;
+      this.heatmap = null;
+         
+  }
+
+}
+
